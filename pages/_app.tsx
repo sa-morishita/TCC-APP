94b0b606aecf8c825ee066e6ef7d7e6f39a9fe2a
Satoshi Morishita <morishita@q-3.jp>
2023-01-05 10:43:30 +0900

    デバッグ用の設定追加、stylesのフォルダ変更

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,4 @@
-import "../styles/globals.scss";
+import "../common/styles/globals.scss";
 import type { AppProps } from "next/app";
 import { Toaster } from "react-hot-toast";
 import { ReactElement, ReactNode } from "react";
