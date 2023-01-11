955e71f4403a0482ae68747a9bae7273d84fd769
Satoshi Morishita <morishita@q-3.jp>
2023-01-11 21:58:37 +0900

    2023/01/16の終了予定時点まで実装

--- components/task/TaskPage.tsx
+++ components/task/TaskPage.tsx
@@ -1,7 +1,66 @@
-import { FC } from "react";
+import { FC, useEffect, useState } from "react";
+import Task from "./ui/Task";
+import { toast } from "react-hot-toast";
 
 const TaskPage: FC = () => {
-  return <div>TaskPage</div>;
+  const [input, setInput] = useState<any>("");
+  const [taskArray, setTaskArray] = useState<any[]>([]);
+
+  useEffect(() => {
+    // タスクが2つdoneになったらアラートを出す
+    const doneTaskArray = taskArray.filter((task) => {
+      return task.done;
+    });
+    if (doneTaskArray.length === 2) {
+      toast.success("お疲れ様です。タスクが2つ完了しました。");
+    }
+  }, [taskArray]);
+
+  // inputの値を取得してsetInputに入れる
+  const handleChange = (e: any) => {
+    setInput(e.target.value);
+  };
+
+  // inputの値をtaskArrayに入れる
+  const handleClick = () => {
+    setTaskArray([...taskArray, { title: input, done: false }]);
+    toast.success("タスクを追加しました");
+    setInput("");
+  };
+
+  return (
+    <div className="mt-4 flex w-full items-center justify-center">
+      <div className="w-full rounded bg-white p-6 shadow lg:max-w-3xl">
+        <div className="mt-4 mb-8 flex">
+          <input
+            className="mr-4 w-full appearance-none rounded border py-2 px-3 text-gray-900 shadow"
+            placeholder="ここに記入"
+            value={input}
+            onChange={handleChange}
+          />
+          <button
+            className="flex-shrink-0 rounded border-2 border-teal-500 p-2 text-teal-500 hover:bg-teal-500 hover:text-white"
+            type="button"
+            onClick={() => handleClick()}
+          >
+            タスクを追加
+          </button>
+        </div>
+        <div>
+          {taskArray.map((task, index) => {
+            return (
+              <Task
+                task={task}
+                taskArray={taskArray}
+                setTaskArray={setTaskArray}
+                key={task + String(index)}
+              />
+            );
+          })}
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default TaskPage;
---
+++ components/task/ui/Task.tsx
@@ -0,0 +1,64 @@
+import { FC, memo } from "react";
+import { toast } from "react-hot-toast";
+
+interface Props {
+  task: any;
+  taskArray: any[];
+  setTaskArray: any;
+}
+
+const Task: FC<Props> = ({ task, taskArray, setTaskArray }) => {
+  const { title, done } = task;
+
+  // タスクを完了にする
+  const handleDone = () => {
+    const newTaskArray = taskArray.map((task) => {
+      if (task.title === title) {
+        return { ...task, done: !done };
+      }
+      return task;
+    });
+    setTaskArray(newTaskArray);
+  };
+
+  // タスクを削除する
+  const handleDelete = () => {
+    const newTaskArray = taskArray.filter((task) => {
+      return task.title !== title;
+    });
+    setTaskArray(newTaskArray);
+    toast.success("タスクを削除しました");
+  };
+
+  return (
+    <div className="mb-4 flex items-center">
+      <p
+        className={`w-full ${
+          done ? "text-green-500 line-through" : "text-gray-900"
+        }`}
+      >
+        {title}
+      </p>
+      <button
+        type="button"
+        onClick={() => handleDone()}
+        className={`ml-4 mr-2 flex-shrink-0 rounded border-2 p-2 ${
+          done
+            ? "border-gray-500 text-gray-500 hover:bg-gray-500 hover:text-white"
+            : "border-green-500 text-green-500 hover:bg-green-500 hover:text-white"
+        }`}
+      >
+        {done ? "戻す" : "完了"}
+      </button>
+      <button
+        type="button"
+        onClick={() => handleDelete()}
+        className="ml-2 flex-shrink-0 rounded border-2 border-red-500 p-2 text-red-500 hover:bg-red-500 hover:text-white"
+      >
+        削除
+      </button>
+    </div>
+  );
+};
+
+export default memo(Task);
